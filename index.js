4ec26fa9ecab304077e27273e686a635e1bcd394
Nathan Buchar <nathan.buchar@nurun.com>
2015-08-31 19:25:23 -0700

    Various enhanements to project structure and public-facing API

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-module.exports = require('./lib/cipher');
+/**
+ * Public node-cipher encryption and decryption API.
+ *
+ * @module main
+ * @author Nathan Buchar
+ * @since 2.3.0
+ */
+
+var crypto = require('crypto');
+
+var cipher = require('./lib/cipher');
+
+/**
+ * Encrypts the file without a prompt, given that the password is passed in as
+ * a function parameter.
+ *
+ * @param {Object} options - Cipher options.
+ * @param {Function} [callback] - Callback function.
+ * @returns {Promise}
+ * @access public
+ */
+module.exports.encrypt = function (options, callback) {
+  return cipher.xcrypt(options, crypto.createCipher, callback);
+};
+
+/**
+ * Decrypts the file without a prompt, given that the password is passed in as
+ * a function parameter.
+ *
+ * @param {Object} options - Cipher options.
+ * @param {Function} [callback] - Callback function.
+ * @returns {Promise}
+ * @access public
+ */
+module.exports.decrypt = function (options, callback) {
+  return cipher.xcrypt(options, crypto.createDecipher, callback);
+};
